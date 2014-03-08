0e8e24c74721a081027ac250e29962827b0502b6
Nate Brennand <natebrennand@gmail.com>
2014-03-08 15:33:45 -0500

    format

--- change/change.go
+++ change/change.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func GetChange (tendered, cost int) map[string]int {
+func GetChange(tendered, cost int) map[string]int {
 	change := make(map[string]int)
 
 	diff := tendered - cost
@@ -18,56 +18,56 @@ func GetChange (tendered, cost int) map[string]int {
 				change["twenty"] = 0
 			}
 			change["twenty"]++
-			diff  -= 2000
+			diff -= 2000
 		case diff >= 1000:
 			_, ok := change["ten"]
 			if !ok {
 				change["ten"] = 0
 			}
 			change["ten"]++
-			diff  -= 1000
+			diff -= 1000
 		case diff >= 500:
 			_, ok := change["five"]
 			if !ok {
 				change["five"] = 0
 			}
 			change["five"]++
-			diff  -= 500
+			diff -= 500
 		case diff >= 100:
 			_, ok := change["one"]
 			if !ok {
 				change["one"] = 0
 			}
 			change["one"]++
-			diff  -= 100
+			diff -= 100
 		case diff >= 25:
 			_, ok := change["quarter"]
 			if !ok {
 				change["quarter"] = 0
 			}
 			change["quarter"]++
-			diff  -= 25
+			diff -= 25
 		case diff >= 10:
 			_, ok := change["dime"]
 			if !ok {
 				change["dime"] = 0
 			}
 			change["dime"]++
-			diff  -= 10
+			diff -= 10
 		case diff >= 5:
 			_, ok := change["nickel"]
 			if !ok {
 				change["nickel"] = 0
 			}
 			change["nickel"]++
-			diff  -= 5
+			diff -= 5
 		case diff >= 1:
 			_, ok := change["penny"]
 			if !ok {
 				change["penny"] = 0
 			}
 			change["penny"]++
-			diff  -= 1
+			diff -= 1
 		default:
 			return change
 		}
@@ -80,7 +80,7 @@ func Usuage() {
 	os.Exit(1)
 }
 
-func CliArgs() (int, int){
+func CliArgs() (int, int) {
 	if len(os.Args) != 3 {
 		Usuage()
 	}
@@ -89,15 +89,14 @@ func CliArgs() (int, int){
 		fmt.Println(err.Error())
 		Usuage()
 	}
-	tendered := int(100*tendered_float)
+	tendered := int(100 * tendered_float)
 
 	cost_float, err := strconv.ParseFloat(os.Args[2], 64)
 	if err != nil {
 		fmt.Println(err.Error())
 		Usuage()
 	}
-	cost := int(100*cost_float)
-
+	cost := int(100 * cost_float)
 
 	if tendered < cost {
 		fmt.Println("You need to pay more than the item costs!")
