e1500446e395fb758cfb8365008147a0fb2d543a
Nate Brennand <natebrennand@gmail.com>
2014-04-01 17:51:15 -0400

    speed upgrade w/ channels
    closes #1

--- collatz/collatz.go
+++ collatz/collatz.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"runtime"
 	"math"
 )
 
 const MAX_POWER = 7
+
+type Result struct {
+	num, count int
+}
 
 // Collatz Conjecture
 // Start with a number n > 1.
 // Find the number of steps it takes to reach one using the following process:
 //		If n is even, divide it by 2.
 // 		If n is odd, multiply it by 3 and add 1.
-
 func collatz(n, count int) int {
 	if n == 1 {
 		return count
@@ -25,25 +29,43 @@ func collatz(n, count int) int {
 	}
 }
 
-func maxCollatz() {
-	n, maxC := 1, 0
-	var i int
-	for i=1; i<int(math.Pow10(MAX_POWER)); i++ {
+func maxCollatzRangeWorker(s int, max chan Result) {
+	var n int
+	magnitude := int(math.Pow10(MAX_POWER-1))
+	maxC := 0
+	for i:= s*magnitude ; i<(s+1)*magnitude; i++ {
 		temp := collatz(i, 0)
 		if temp > maxC {
 			n = i
 			maxC = temp
-			fmt.Println(n, maxC)
 		}
 	}
-	fmt.Printf("%d is the number with the most collatz iterations between 1 and %d with %d\n", n, int(math.Pow10(MAX_POWER)), maxC)
+	max <-  Result{n, maxC}
+}
+
+func maxCollatz() {
+	var maxNum, maxCount int
+	maxs := make(chan Result)
+	for i:=1; i<10; i++ {
+		go maxCollatzRangeWorker(i, maxs)
+	}
+
+	for i:=1; i<10; i++ {
+		r := <-maxs
+		if r.count > maxCount {
+			maxCount = r.count
+			maxNum = r.num
+		}
+	}
+
+	fmt.Printf("%d is the number with the most collatz iterations between 1 and %d with %d\n", maxNum, int(math.Pow10(MAX_POWER)), maxCount)
 }
 
 func main() {
 	if len(os.Args) != 2 {
+		runtime.GOMAXPROCS(11)
 		maxCollatz()
 	} else {
-
 		n, err := strconv.Atoi(os.Args[1])
 		if err != nil {
 			fmt.Println("You must pass in an integer as an argument!\n")
